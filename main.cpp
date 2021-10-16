3376de2f9d5f6e957a4fe0d157b7079ab536a623
EnriqueLemus <83040316+EnriqueLemus@users.noreply.github.com>
2021-10-15 23:07:09 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//Luis Enrique Lemus Martinez A01639698
+//Actividad Integral de Conceptos Básicos y Algoritmos Fundamentales
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <vector>
+//#include "Node.h"
+//#include "Stack.h"
+#include "Registro.h"
+using namespace std;
+
+
+int main(){
+    Registro bitacora;
+
+    bitacora.archivo();
+    bitacora.archivoOrdenado();
+    
+    
+}
